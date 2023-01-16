24ac11ed78368eadcb97c8ea6bf2736623bd26f2
Mussa Justin Muhindo <jmussamuhindo@gmail.com>
2023-01-16 16:10:13 +0100

    project1

---
+++ gameoflife/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace gameoflife
+{
+    partial class gameoflifemain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbGrid = new System.Windows.Forms.PictureBox();
+            this.numSSize = new System.Windows.Forms.NumericUpDown();
+            this.btnRest = new System.Windows.Forms.Button();
+            this.btnAdvance = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGrid
+            // 
+            this.pbGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbGrid.Location = new System.Drawing.Point(21, 61);
+            this.pbGrid.Name = "pbGrid";
+            this.pbGrid.Size = new System.Drawing.Size(1097, 493);
+            this.pbGrid.TabIndex = 0;
+            this.pbGrid.TabStop = false;
+            this.pbGrid.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // numSSize
+            // 
+            this.numSSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numSSize.Location = new System.Drawing.Point(135, 585);
+            this.numSSize.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numSSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSSize.Name = "numSSize";
+            this.numSSize.Size = new System.Drawing.Size(67, 26);
+            this.numSSize.TabIndex = 1;
+            this.numSSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnRest
+            // 
+            this.btnRest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRest.Location = new System.Drawing.Point(228, 577);
+            this.btnRest.Name = "btnRest";
+            this.btnRest.Size = new System.Drawing.Size(97, 41);
+            this.btnRest.TabIndex = 3;
+            this.btnRest.Text = "Reset";
+            this.btnRest.UseVisualStyleBackColor = true;
+            this.btnRest.Click += new System.EventHandler(this.btnRest_Click);
+            // 
+            // btnAdvance
+            // 
+            this.btnAdvance.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnAdvance.Location = new System.Drawing.Point(818, 578);
+            this.btnAdvance.Name = "btnAdvance";
+            this.btnAdvance.Size = new System.Drawing.Size(98, 41);
+            this.btnAdvance.TabIndex = 4;
+            this.btnAdvance.Text = "Advance";
+            this.btnAdvance.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnGo.Location = new System.Drawing.Point(941, 578);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(81, 41);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 583);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "cell size";
+            // 
+            // gameoflifemain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 636);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnAdvance);
+            this.Controls.Add(this.btnRest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numSSize);
+            this.Controls.Add(this.pbGrid);
+            this.MinimumSize = new System.Drawing.Size(960, 620);
+            this.Name = "gameoflifemain";
+            this.Text = "game of life";
+            this.Load += new System.EventHandler(this.gameoflifemain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGrid;
+        private System.Windows.Forms.NumericUpDown numSSize;
+        private System.Windows.Forms.Button btnRest;
+        private System.Windows.Forms.Button btnAdvance;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ gameoflife/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gameoflife
+{
+    public partial class gameoflifemain : Form
+    {
+        public gameoflifemain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameoflifemain_Load(object sender, EventArgs e)
+        {
+
+            createGridSurface();
+        }
+
+        private void createGridSurface()
+        {
+            Point locPoint;
+            Cell newCell;
+            Random random = new Random();
+
+            int rows = (int)(pbGrid.Height / numSSize.Value);
+            int cols = (int)(pbGrid.Height/numSSize.Value);
+
+
+            using (Bitmap bmp = new Bitmap(pbGrid.Width, pbGrid.Height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (SolidBrush cellBrush = new SolidBrush(Color.DarkOrange))
+            {
+                g.Clear(Color.Black);
+                pbGrid.Image = (Bitmap)bmp;
+
+                for (int y = 0; y < rows; y++)
+                {
+                    for (int x = 0; x < cols; x++)
+                    {
+                        locPoint = new Point((int)(x * numSSize.Value), (int)(y * numSSize.Value));
+                        newCell = new Cell(Location, x, y);
+                        newCell.IsAlive = (random.Next(100) < 15) ? true : false;
+                        Cell.gridCells.Add(newCell);
+                    }
+                }
+
+                        //plot all the nemly created cells to the grid
+                        foreach(Cell cell in Cell.gridCells)
+                        {
+                            if (cell.IsAlive)
+                            {
+                                g.FillRectangle(cellBrush, new Rectangle(cell.Location, 
+                                    new Size((int)numSSize.Value - 1, (int)numSSize.Value-1)));
+                            }
+                        }
+                        pbGrid.Image = (Bitmap)bmp.Clone();
+                    }
+                }
+
+        private void btnRest_Click(object sender, EventArgs e)
+        {
+            createGridSurface();
+        }
+    }
+        }
+    
+
+    public class Cell
+    {
+        public static List<Cell> gridCells = new List<Cell>();
+        private Point cLocation;
+        private int cXPos;
+        private int cYPos;
+        private Boolean cIsAlive;
+
+        public Cell(Point location, int X, int Y)
+        {
+            this.Location = location;
+        }
+
+        public Point Location
+        {
+            get { return cLocation; }
+            set { cLocation = value; }
+        }
+        public int XPos
+        {
+            get { return cXPos; }
+            set { cXPos = value; }
+        }
+        public int YPos
+        {
+            get { return cYPos; }
+            set { cYPos = value; }
+        }
+        public Boolean IsAlive
+        {
+            get { return cIsAlive; }
+            set { cIsAlive = value; }
+        }
+    }
+
+
